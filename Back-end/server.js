4d2a5330caf688d44c211e82d91d396f8597b10b
Tom van den Berg <tomvandenberg11@hotmail.com>
2021-04-16 13:20:35 +0200

    adding routing

--- Back-end/server.js
+++ Back-end/server.js
@@ -2,12 +2,18 @@ var _ = require('lodash');
 const express = require('express')
 const app = express()
 
-const hallo = ['hi', 'hoi', 'doei']
-
-console.log(_.find(hallo))
+const hallo = ['Home', 'About', 'Contact']
 
 app.get('/', function (req, res) {
-    res.send('yoooo')
+    res.send(hallo[0].toString() + ' page')
+})
+
+app.get('/about', function (req, res) {
+    res.send(hallo[1].toString() + ' page')
+})
+
+app.get('/contact', function (req, res) {
+    res.send(hallo[2].toString() + ' page')
 })
 
 app.listen(3000)
