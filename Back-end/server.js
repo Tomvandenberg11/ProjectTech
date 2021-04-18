285de0a6fb105a691aa08cda151a2f7e32bbc53d
Tom van den Berg <tomvandenberg11@hotmail.com>
2021-04-18 13:00:35 +0200

    adding routing, gitignore and wiki sidebar

--- Back-end/server.js
+++ Back-end/server.js
@@ -2,18 +2,18 @@ var _ = require('lodash');
 const express = require('express')
 const app = express()
 
-const hallo = ['Home', 'About', 'Contact']
+const page = ['Home', 'About', 'Contact']
 
 app.get('/', function (req, res) {
-    res.send(hallo[0].toString() + ' page')
+    res.send(page[0].toString() + ' page')
 })
 
 app.get('/about', function (req, res) {
-    res.send(hallo[1].toString() + ' page')
+    res.send(page[1].toString() + ' page')
 })
 
 app.get('/contact', function (req, res) {
-    res.send(hallo[2].toString() + ' page')
+    res.send(page[2].toString() + ' page')
 })
 
 app.listen(3000)
