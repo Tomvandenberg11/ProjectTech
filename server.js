216aef5187eb1e6af1d460730433c38e809d4832
Tom van den Berg <tomvandenberg11@hotmail.com>
2021-04-19 20:19:59 +0200

    installing pug and adding template file

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 var _ = require('lodash');
 const express = require('express')
 const app = express()
+app.set('view engine', 'pug');
 
-const page = ['Home', 'About', 'Contact']
-
+// Root pagina
 app.get('/', function (req, res) {
-    res.send(page[0].toString() + ' page')
+    res.render('index', { title: 'Template engine werkt', message: 'Hello there!' })
 })
 
 app.get('/about', function (req, res) {
-    res.send(page[1].toString() + ' page')
+    res.send('About page')
 })
 
-app.get('/contact', function (req, res) {
-    res.send(page[2].toString() + ' page')
+app.get('/results', function (req, res) {
+    res.send('Result page')
+})
+
+// 404 pagina
+app.use((req, res, next) => {
+    res.send('Deze pagina bestaat niet!')
 })
 
 app.listen(3000)
