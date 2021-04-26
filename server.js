5abcb626243c54fcd401565474f0f459632bc387
Tom van den Berg <tomvandenberg11@hotmail.com>
2021-04-26 15:06:42 +0200

    adding image, style and styling 404 page

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const _ = require('lodash');
 const express = require('express')
 const app = express()
-const port = 6969
+const port = 3000
 
 app.use(express.static('static'))
 app.set('view engine', 'ejs');
 
 // Root pagina
 app.get('/', function (req, res) {
-    res.render('index', { title: 'Travel Buddy | Home', message: 'Welkom!!' })
+    res.render('index', { title: 'Travel Buddy | Home', message: 'Welkom!' })
 })
 
+// Result pagina
 app.get('/results', function (req, res) {
-    res.render('results', { title: 'Travel Buddy | Results', message: 'Resultaten!!' })
+    res.render('results', { title: 'Travel Buddy | Results', message: 'Resultaten!' })
 })
 
 // 404 pagina
 app.use((req, res, next) => {
-    res.send('Deze pagina bestaat niet!')
+    res.render('results', { title: 'Travel Buddy | 404', message: 'Deze pagina bestaat niet!' })
 })
 
 app.listen(port)
