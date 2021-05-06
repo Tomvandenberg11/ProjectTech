085d6d9475198430c3fe0ace3ec841a9453265a3
Tom van den Berg <tomvandenberg11@hotmail.com>
2021-05-06 12:05:35 +0200

    trying database

--- server.js
+++ server.js
@@ -2,23 +2,47 @@ const _ = require('lodash');
 const express = require('express')
 const app = express()
 const port = 3000
+const {MongoClient} = require('mongodb');
 
-app.use(express.static('static'))
-app.set('view engine', 'ejs');
+require('dotenv').config()
+
+const {
+    MONGO_USER,
+    MONGO_PASS,
+    MONGO_URI,
+} = process.env
+
+main()
+
+function main() {
+
+    MongoClient
+    .connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_URI}?retryWrites=true&w=majority`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+
+
+
+
+    app.use(express.static('static'))
+    app.set('view engine', 'ejs');
 
 // Root pagina
-app.get('/', function (req, res) {
-    res.render('index', { title: 'Travel Buddy | Home', message: 'Welkom!' })
-})
+    app.get('/', function (req, res) {
+        res.render('index', { title: 'Travel Buddy | Home', message: 'Welkom!', numbers: 5 })
+    })
 
 // Result pagina
-app.get('/results', function (req, res) {
-    res.render('results', { title: 'Travel Buddy | Results', message: 'Resultaten!' })
-})
+    app.get('/results', function (req, res) {
+        res.render('results', { title: 'Travel Buddy | Results', message: 'Resultaten!' })
+    })
 
 // 404 pagina
-app.use((req, res, next) => {
-    res.render('results', { title: 'Travel Buddy | 404', message: 'Deze pagina bestaat niet!' })
-})
+    app.use((req, res, next) => {
+        res.render('results', { title: 'Travel Buddy | 404', message: 'Deze pagina bestaat niet!' })
+    })
 
-app.listen(port)
+    app.listen(port, () => console.log(`De server is opgestart op http://localhost:${port}`))
+}
+
