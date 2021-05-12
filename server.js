2c532d3b4a842b51279b17c224d04e454ca7afb0
Tom van den Berg <tomvandenberg11@hotmail.com>
2021-05-12 12:05:08 +0200

    adding database connection

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const _ = require('lodash');
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 6969
 const {MongoClient} = require('mongodb');
 
-require('dotenv').config()
+require('dotenv').config({path: '.env-dev'})
 
 const {
     MONGO_USER,
@@ -15,34 +15,55 @@ const {
 main()
 
 function main() {
-
     MongoClient
-    .connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_URI}?retryWrites=true&w=majority`, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+
+      // Database connectie
+      .connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_URI}?retryWrites=true&w=majority`, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+      })
+
+      // Promise, als de connectie er is voert het deze dingen uit
+    .then(connection => {
+
+        let destinations = {
+          country: '',
+          capital: '',
+          image: '',
+          days: 0,
+          type: '',
+          likes: 0,
+          liked: false
+        }
+
+        const db = connection.db('TravelBuddy')
+        const collection = db.collection('destinations')
+
+        app.set('view engine', 'ejs')
+
+        app.use(express.static('static'))
+
+        app.get('/', (req, res) => {
+          collection.find().toArray()
+            .then(result => {
+                res.render('index.ejs', {
+                  destinations: result,
+                  capital: destinations,
+                })
+            })
+            .catch(error => console.error(error))
         })
 
+      app.post('/', function(req, res) {
+        collection.updateOne(
+          {_id: this._id},
+          {"$set": { "liked" : true},
+           "$inc": { "likes" : 1}}
+        );
+        console.log(collection.likes)
+      });
 
-
-
-    app.use(express.static('static'))
-    app.set('view engine', 'ejs');
-
-// Root pagina
-    app.get('/', function (req, res) {
-        res.render('index', { title: 'Travel Buddy | Home', message: 'Welkom!', numbers: 5 })
+        app.listen(port, () => console.log(`De server is opgestart op http://localhost:${port}`))
     })
-
-// Result pagina
-    app.get('/results', function (req, res) {
-        res.render('results', { title: 'Travel Buddy | Results', message: 'Resultaten!' })
-    })
-
-// 404 pagina
-    app.use((req, res, next) => {
-        res.render('results', { title: 'Travel Buddy | 404', message: 'Deze pagina bestaat niet!' })
-    })
-
-    app.listen(port, () => console.log(`De server is opgestart op http://localhost:${port}`))
 }
 
---
+++ static/js/like.js
@@ -0,0 +1,5 @@
+document.querySelector('#like').forEach(button => button.addEventListener('click', dislike))
+
+function dislike() {
+
+}
