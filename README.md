fe5a1c8e520aa602d9a436c0b947f341d73594eb
Tom van den Berg <tomvandenberg11@hotmail.com>
2021-05-19 20:13:20 +0200

    adding linking feature and results page

--- README.md
+++ README.md
@@ -1,8 +1,39 @@
 # Matching App - Travel Buddy
 
-Travel Buddy is een app waarmee de gebruikers van de app een reispartner kunnen vinden, op basis van hun persoonlijke voorkeuren. Er kan bijvoorbeeld gefilterd worden op activiteiten, het type vervoersmiddel, de rijstijd, het klimaat en de veiligheid in een land. 
+Travel Buddy is een app waarmee de gebruikers van de app een reispartner kunnen vinden, op basis van hun persoonlijke voorkeuren. Er kan bijvoorbeeld gefilterd worden op activiteiten, het type vervoersmiddel, de rijstijd, het klimaat en de veiligheid in een land.
 
-### Licence
+## Installatie
+### Clonen
+Om de app lokaal te laten draaien moet het project eerst lokaal worden gecloned.
+Als dit stukje code gerunt wordt in de terminal wordt de repository lokaal gecloned:
+
+`gh repo clone tomvandenberg11/ProjectTech.git`
+
+Ga eerst naar de folder waarin je wilt dat het project gecloned wordt. Je kan in de terminal navigeren met `cd` met daar achter de map waar je heen wilt.
+
+Daarna is het nodig om `node` en `npm` geinstalleerd te hebben op je lokale computer. Als je deze stappen gevolgd hebt, worden met `npm install` de benodigde packages gedownload.
+
+### Database
+De app heeft een database met daarin de data voor de app. Deze app gebruikt daarvoor [MongoDB](https://www.mongodb.com/).
+Je hebt een account nodig om een database aan te maken. Hier kan je dan vervolgens alle gewenste data in zetten.
+Als je een database hebt aangemaakt is het wel nodig om de configuratie te laten werken. In het .env file staan een aantal invoervelden die
+ingevuld moeten worden:
+
+`MONGO_USER=` gebruikersnaam MongoDB database hier
+
+`MONGO_PASS=` wachtwoord MongoDB database hier
+
+`MONGO_URI=` link MongoDB database hier
+
+### Running
+Als je de bovenstaande stappen gevolgd hebt kan de app opgestart worden door:
+
+`npm run server` 
+
+te runnen in de terminal.
+Je ziet dan een bericht in de terminal staan op welke link de app te zien is.
+
+## Licence
 
 Dit project is voorzien van een MIT licence. 
 
--- server.js
+++ server.js
@@ -18,49 +18,80 @@ main()
 function main() {
     MongoClient
 
-      // Database connectie
-      .connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_URI}?retryWrites=true&w=majority`, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-      })
+    // Database connectie
+    .connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_URI}?retryWrites=true&w=majority`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
 
       // Promise, als de connectie er is voert het deze dingen uit
     .then(connection => {
 
-        const db = connection.db('TravelBuddy')
-        const collection = db.collection('destinations')
+      const db = connection.db('TravelBuddy')
+      const collection = db.collection('destinations')
 
-        app.set('view engine', 'ejs')
+      app.set('view engine', 'ejs')
 
-        app.use(express.static('static'))
+      app.use(express.static('static'))
 
-        app.get('/', (req, res) => {
-          collection.find().toArray()
-            .then(result => {
-                res.render('index.ejs', {
-                  destinations: result,
-                })
+      app.get('/', (req, res) => {
+        collection.find().toArray()
+          .then(result => {
+              res.render('index.ejs', {
+                destinations: result,
+              })
+          })
+          .catch(error => console.error(error))
+      })
+
+      app.get('/results', (req, res) => {
+        collection.find().toArray()
+          .then(result => {
+            res.render('results.ejs', {
+              destinations: result,
             })
-            .catch(error => console.error(error))
-        })
+          })
+          .catch(error => console.error(error))
+      })
 
-      app.post('/update', (req, res) => {
+      // DISLIKE DESTINATION
+      app.post('/dislike/*', (req, res) => {
         MongoClient.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_URI}?retryWrites=true&w=majority`, function(err, db) {
           if (err) throw err;
           const dbo = db.db("TravelBuddy");
-          const destination = { country: new ObjectID(req.query.id) };
-          console.log(new ObjectID(req.query.id))
-          const like = { $set: {likes: 1, liked: false } };
-          dbo.collection("destinations").updateOne(destination, like, function(err, res) {
+          const destination = { _id: ObjectID(req.params[0]) };
+          const like = { $set: { liked: false },
+            $inc: {likes: -1}};
+          dbo.collection("destinations").updateOne(destination, like,  function(err, res) {
             if (err) throw err;
-            console.log("1 document updated");
             db.close();
           });
         });
 
-        res.render('update.ejs', {
-          title: 'test page',
-        })
+        setTimeout(() =>{
+          res.redirect('back')
+        }, 800)
+
+      })
+
+      // LIKE DESTINATION
+      app.post('/like/*', (req, res) => {
+        MongoClient.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${MONGO_URI}?retryWrites=true&w=majority`, function(err, db) {
+          if (err) throw err;
+          const dbo = db.db("TravelBuddy");
+          const destination = { _id: ObjectID(req.params[0]) };
+          const like = { $set: { liked: true },
+                         $inc: {likes: 1}};
+          dbo.collection("destinations").updateOne(destination, like,  function(err, res) {
+            if (err) throw err;
+            db.close();
+          });
+        });
+
+        // Timeout zodat de resultaten meteen te zien zijn
+        setTimeout(() => {
+          res.redirect('back')
+        }, 800)
       })
 
       app.listen(port, () => console.log(`De server is opgestart op http://localhost:${port}`))
